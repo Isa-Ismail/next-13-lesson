e800d91ae5a646b6b160106bd4d9839749ecb675
isa-ismail <fahimismail75.scc@gmail.com>
2023-06-26 19:11:19 +0600

    resolve

--- src/app/about/[id]/page.tsx
+++ src/app/about/[id]/page.tsx
@@ -7,6 +7,16 @@ interface Props {
 }
 
 const page: React.FC<Props> = ({ params }) => {
+
+    const fn = () => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('ok')
+        }, 1000)
+    })
+    fn().then((res) => {
+        console.log(res)
+    })
+
     return (
         <div>
             about {params.id}
